ad8493decc9a2d400ce30f5e7b6c3d6a521e4fd7
Praveen Peser <praveenpeser4@gmail.com>
2020-08-24 14:41:09 -0400

    File contains methods to get nth febonacci number by using 1. recursion 2. Loop
    Added perf_counter to compare above methods

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from time import perf_counter
+
+
+def timed(fn):
+    def inner(*args, **kwargs):
+        start = perf_counter()
+        result = fn(*args, **kwargs)
+        end = perf_counter()
+        print("Time taken: {0:.6f} secs".format(end - start))
+        return result
+
+    return inner
 
 
 def get_user_input():
@@ -10,17 +22,23 @@ def get_user_input():
             print("Enter a valid number")
             continue
 
+
 def recurion_fibonacci(num):
     if num <= 2:
         return 1
     else:
-        return recurion_fibonacci(num-2) + recurion_fibonacci(num-1)
+        return recurion_fibonacci(num - 2) + recurion_fibonacci(num - 1)
 
+@timed
+def calc_recurion_fibonacci(num):
+    return recurion_fibonacci(num)
+
+@timed
 def loop_fibonacci(num):
     num1 = 1
     num2 = 1
-    for i in range(3, num+1):
-        num1, num2 = num2, num1+num2
+    for i in range(3, num + 1):
+        num1, num2 = num2, num1 + num2
     return num2
 
 
@@ -28,11 +46,10 @@ if __name__ == "__main__":
     num = get_user_input()
     print("Calculating Fibonacci for number : {0}".format(num))
 
-    #recursion method
-    #feb = recurion_fibonacci(num)
+    # recursion method
+    #feb = calc_recurion_fibonacci(num)
 
-    #using loop
+    # using loop
     feb = loop_fibonacci(num)
 
     print("{0}th Fibonacci number is: {1}".format(num, feb))
-
